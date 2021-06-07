92ec01616f227891937ac7ca3cd5a6c8ffe456da
alphaion <alphaion89@gmail.com>
2021-06-07 13:17:57 +0530

    landing page added

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
+$(document).ready(function () {
+    //Preloader
+    preloaderFadeOutTime = 500;
+
+    function hidePreloader() {
+        var preloader = $('.spinner-wrapper');
+        preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+});
+
 const navSlide = () => {
     const menu = document.querySelector(".menu");
     const nav = document.querySelector(".nav-links");
 
+    // Issue: when menuON bg blurred but with the navigation
+    //const blurBg = document.querySelector("#wrapper");
+    
     const navLinks = document.querySelectorAll('.nav-links li');
 
     menu.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+
+        // blurred class
+        //blurBg.classList.toggle('bg-blur');
 
         navLinks.forEach((link, index) => {
 
