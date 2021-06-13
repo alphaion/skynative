025191482823efda0cd0af201d3d47f83d5d89ff
alphaion <alphaion89@gmail.com>
2021-06-14 02:18:30 +0530

    responsive

--- script/script.js
+++ script/script.js
@@ -15,16 +15,11 @@ const navSlide = () => {
     const menu = document.querySelector(".menu");
     const nav = document.querySelector(".nav-links");
 
-    // Issue: when menuON bg blurred but with the navigation
-    //const blurBg = document.querySelector("#wrapper");
-
     const navLinks = document.querySelectorAll('.nav-links li');
 
     menu.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
-        // blurred class
-        //blurBg.classList.toggle('bg-blur');
 
         navLinks.forEach((link, index) => {
 
